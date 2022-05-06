1cb7f460e8ffa59a7f8e3916347a4dd8856dc894
Kyle Stenersen <k.stenersen21@gmail.com>
2022-05-05 17:12:26 -0700

    remove accidental main_form_load function

--- Nameplate_GUI/Form1.Designer.cs
+++ Nameplate_GUI/Form1.Designer.cs
@@ -189,7 +189,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = " DU Nameplate GUI";
             this.TopMost = true;
-            this.Load += new System.EventHandler(this.MAIN_FORM_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
